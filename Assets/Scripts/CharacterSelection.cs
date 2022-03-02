85e96aafa69540684a7705338cb9386070cb2639
ariel <ariel7590@gmail.com>
2022-03-02 22:52:42 +0200

    Added character models to the roster (4 fighters don't work with animation beacuse of the stupid creators of the fighters pack

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 public class CharacterSelection : MonoBehaviour
 {
     [SerializeField] private Outline imageOutline;
+    [SerializeField] private GameObject model;
     private Color color;
+    private bool isInstanceExists;
+    private Object modelClone;
 
     // Start is called before the first frame update
     void Start()
     {
         color = imageOutline.effectColor;
+        isInstanceExists = false;
     }
 
     // Update is called once per frame
@@ -23,10 +27,20 @@ public class CharacterSelection : MonoBehaviour
     public void ChangeColorWhenHovering()
     {
         imageOutline.effectColor = Color.red;
+        if(!isInstanceExists)
+        {
+            modelClone=Instantiate(model);
+            isInstanceExists = true;
+        }
     }
 
     public void ChangeColorBackWhenNotHovering()
     {
         imageOutline.effectColor = color;
+        if(isInstanceExists)
+        {
+            Destroy(modelClone);
+            isInstanceExists = false;
+        }
     }
 }
