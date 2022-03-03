d1b63dfad500a9713d48502780ea94513ab4f3b7
ariel <ariel7590@gmail.com>
2022-03-03 20:52:28 +0200

    Added the character selection to the main menu (in a special way)

--- Assets/Scripts/GameplayMode.cs
+++ Assets/Scripts/GameplayMode.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameplayMode : MonoBehaviour
 {
     [SerializeField] private GameObject menuButtonSet;
     [SerializeField] private AudioSource introSong;
+    private AudioSource buttonClick; // Not the regular click but the voice that saying "Welcome to the olympus tournament"
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        buttonClick = GameObject.Find("Gameplay Button Set").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!introSong.isPlaying && !buttonClick.isPlaying)
+        {
+            SceneManager.LoadScene(2); // Scene index 2 is character selection scene
+        }
     }
 
     public void CloseWindow()
@@ -28,6 +33,7 @@ public class GameplayMode : MonoBehaviour
     public void ML1()
     {
         introSong.Stop();
+        buttonClick.Play();
     }
     public void ML2()
     {
