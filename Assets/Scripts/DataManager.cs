4ee9e1b9d5b64333da972f30316f4070fce57598
ariel <ariel7590@gmail.com>
2022-03-04 20:19:31 +0200

    Added data manager file

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    // Like Start but is called even if the sctipt is disabled
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
