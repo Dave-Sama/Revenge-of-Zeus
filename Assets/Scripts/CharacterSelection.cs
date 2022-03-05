281714fe397efd8717cf85662e8b4273e244b65f
ariel <ariel7590@gmail.com>
2022-03-05 02:38:33 +0200

    made a new panel and working on the data flow to the battle scene

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -8,15 +8,18 @@ public class CharacterSelection : MonoBehaviour
 {
     [SerializeField] private Outline imageOutline;
     [SerializeField] private GameObject model;
+    [SerializeField] private GameObject areYouSurePanel;
     private Color color;
     private bool isInstanceExists;
     private Object modelClone;
+    private AudioSource nameSound;
 
     // Start is called before the first frame update
     void Start()
     {
         color = imageOutline.effectColor;
         isInstanceExists = false;
+        nameSound = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -48,5 +51,21 @@ public class CharacterSelection : MonoBehaviour
     public void onBackBtnClick()
     {
         SceneManager.LoadScene(0); //index 0 = main menu scene
+    }
+
+    public void onCharacterClick()
+    {
+        areYouSurePanel.SetActive(true);
+        
+    }
+
+    public void PressNoOnPanel()
+    {
+        areYouSurePanel.SetActive(false);
+    }
+
+    public void PressYesOnPanel()
+    {
+        SceneManager.LoadScene(3); //index 3 = battle scene
     }
 }
