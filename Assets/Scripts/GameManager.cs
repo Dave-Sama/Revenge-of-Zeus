7364f015941355a2097526f1906ff9185ce6e4db
ariel <ariel7590@gmail.com>
2022-03-09 21:10:37 +0200

    Created a health bar

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,18 +8,19 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] characters = new GameObject[15];
     private string characterName;
+    private Vector3 characterPosition = new Vector3(-2.13f, 0, 0);
+    //private Quaternion characterRotation = new Quaternion(0, 90, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         characterName = DataManager.Instance.Character;
-        Debug.Log(characterName);
         foreach(GameObject character in characters)
         {
             Debug.Log(character.name);
             if(string.Equals(characterName,character.name))
             {
-                Instantiate(character);
+                Instantiate(character,characterPosition,character.transform.rotation);
             }
         }
     }
