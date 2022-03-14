d49c1c62d6b45e33cfe35d23ad3c7eab87445e85
Dave-Sama <davech1228@gmail.com>
2022-03-14 17:49:02 +0200

    added a background volume change by user

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class Settings : MonoBehaviour
 {
+    public AudioSource AudioSource;
+
+    private float musicVolume = 1f;
     [SerializeField] private GameObject menuButtonSet;
     // Start is called before the first frame update
     void Start()
     {
-        
+  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AudioSource.volume = musicVolume;
+    }
+
+    public void updateVolume(float volume)
+    {
+        musicVolume = volume;
     }
 
     public void CloseWindow()
