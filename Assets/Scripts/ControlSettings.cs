fd013261d8a68c224fb0f5bc655770578ee49668
Dave-Sama <davech1228@gmail.com>
2022-04-07 22:18:30 +0300

    fixed the navigation between settings

--- Assets/Scripts/ControlSettings.cs
+++ Assets/Scripts/ControlSettings.cs
@@ -2,58 +2,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
-public class ControllSettings : MonoBehaviour
+public class ControlSettings : MonoBehaviour
 {
     // player 1
-    private string punch1 = "Q";
-    private string kick1 = "W";
-    private string jump1 = "E";
-    private string bend1 = "R";
-    public InputField punch1InputField;
-    public InputField kick1InputField;
-    public InputField jump1InputField;
-    public InputField bend1InputField;
+    public InputField upper_left_punch1_Field;
+    public InputField upper_right_punch1_Field;
+    public InputField upper_kick1_Field;
+    public InputField middle_left_punch1_Field;
+    public InputField middle_right_punch1_Field;
+    public InputField middle_kick1_Field;
+    public InputField special_attack1_Field;
+    public InputField jump1_Field;
+    public InputField bend1_Field;
+    public InputField block1_Field;
+    private string[] controls = { "A", "S", "D", "Z", "X", "C", "V", "1", "2", "3", "I", "O", "P", "J", "K", "L", ";", "0", "-", "=" };
 
     // player 2
-    private string punch2 = "O";
-    private string kick2 = "P";
-    private string jump2 = "{";
-    private string bend2 = "}";
-    public InputField punch2InputField;
-    public InputField kick2InputField;
-    public InputField jump2InputField;
-    public InputField bend2InputField;
+    public InputField upper_left_punch2_Field;
+    public InputField upper_right_punch2_Field;
+    public InputField upper_kick2_Field;
+    public InputField middle_left_punch2_Field;
+    public InputField middle_right_punch2_Field;
+    public InputField middle_kick2_Field;
+    public InputField special_attack2_Field;
+    public InputField jump2_Field;
+    public InputField bend2_Field;
+    public InputField block2_Field;
 
     // game objects
 
     [SerializeField] private GameObject mainSettings;
     [SerializeField] private GameObject controlSettings;
+    [SerializeField] private GameObject soundSettingsWindow;
+    [SerializeField] private GameObject videoSettingsWindow;
 
     // Start is called before the first frame update
     void Start()
     {
         //Changes the character limit in the main input field.
-        punch1InputField.characterLimit = 1;
-        kick1InputField.characterLimit = 1;
-        jump1InputField.characterLimit = 1;
-        bend1InputField.characterLimit = 1;
-        punch2InputField.characterLimit = 1;
-        kick2InputField.characterLimit = 1;
-        jump2InputField.characterLimit = 1;
-        bend2InputField.characterLimit = 1;
+        upper_left_punch2_Field.characterLimit = 1;
+        upper_right_punch2_Field.characterLimit = 1;
+        upper_kick2_Field.characterLimit = 1;
+        middle_left_punch2_Field.characterLimit = 1;
+        middle_right_punch2_Field.characterLimit = 1;
+        middle_kick2_Field.characterLimit = 1;
+        special_attack2_Field.characterLimit = 1;
+        jump2_Field.characterLimit = 1;
+        bend2_Field.characterLimit = 1;
+        block2_Field.characterLimit = 1;
+
+        upper_left_punch2_Field.characterLimit = 1;
+        upper_right_punch2_Field.characterLimit = 1;
+        upper_kick2_Field.characterLimit = 1;
+        middle_left_punch2_Field.characterLimit = 1;
+        middle_right_punch2_Field.characterLimit = 1;
+        middle_kick2_Field.characterLimit = 1;
+        special_attack2_Field.characterLimit = 1;
+        jump2_Field.characterLimit = 1;
+        bend2_Field.characterLimit = 1;
+        block2_Field.characterLimit = 1;
+
 
         // init player 1 inputs
-        punch1InputField.text = "Q";
-        kick1InputField.text = "W";
-        jump1InputField.text = "E";
-        bend1InputField.text = "R";
+        upper_left_punch1_Field.text = controls[0];
+        upper_right_punch1_Field.text = controls[1];
+        upper_kick1_Field.text = controls[2];
+        middle_left_punch1_Field.text = controls[3];
+        middle_right_punch1_Field.text = controls[4];
+        middle_kick1_Field.text = controls[5];
+        special_attack1_Field.text = controls[6];
+        jump1_Field.text = controls[7];
+        bend1_Field.text = controls[8];
+        block1_Field.text = controls[9];
+
 
         // init player 2 inputs
-        punch2InputField.text = "O";
-        kick2InputField.text = "P";
-        jump2InputField.text = "{";
-        bend2InputField.text = "}";
+        upper_left_punch2_Field.text = controls[10];
+        upper_right_punch2_Field.text = controls[11];
+        upper_kick2_Field.text = controls[12];
+        middle_left_punch2_Field.text = controls[13];
+        middle_right_punch2_Field.text = controls[14];
+        middle_kick2_Field.text = controls[15];
+        special_attack2_Field.text = controls[16];
+        jump2_Field.text = controls[17];
+        bend2_Field.text = controls[18];
+        block2_Field.text = controls[19];
     }
 
     // Go back to main menu
@@ -63,191 +98,218 @@ public class ControllSettings : MonoBehaviour
         controlSettings.SetActive(false);
     }
 
+    // Go to Sound settings
+    public void GoToSoundSettings()
+    {
+        controlSettings.SetActive(false);
+        soundSettingsWindow.SetActive(true);
+    }
+
+    // Go to Video settings
+    public void GoToVideoSettings()
+    {
+
+        controlSettings.SetActive(false);
+        videoSettingsWindow.SetActive(true);
+    }
+
+    // Get upper case substring
+    private string GetUpperSub(string s)
+    {
+        if (s.Length > 1) return s.Substring(0, 1).ToUpper();
+        else return s.ToUpper();
+    }
 
     // Restore all Controllers to default state.
     public void RestoreControllers()
     {
-        ReadPlayer1PunchInput("Q");
-        ReadPlayer1KickInput("W");
-        ReadPlayer1JumpInput("E");
-        ReadPlayer1BendInput("R");
-        ReadPlayer2PunchInput("O");
-        ReadPlayer2KickInput("P");
-        ReadPlayer2JumpInput("{");
-        ReadPlayer2BendInput("}");
+        ReadPlayer1UpperLeftPunchInput(controls[0]);
+        ReadPlayer1UpperRightPunchInput(controls[1]);
+        ReadPlayer1UpperKickInput(controls[2]);
+        ReadPlayer1MiddleLeftPunchInput(controls[3]);
+        ReadPlayer1MiddleRightPunchInput(controls[4]);
+        ReadPlayer1MiddleKickInput(controls[5]);
+        ReadPlayer1SpecialAttackInput(controls[6]);
+        ReadPlayer1JumpInput(controls[7]);
+        ReadPlayer1BendInput(controls[8]);
+        ReadPlayer1BlockInput(controls[9]);
+
+        ReadPlayer2UpperLeftPunchInput(controls[10]);
+        ReadPlayer2UpperRightPunchInput(controls[11]);
+        ReadPlayer2UpperKickInput(controls[12]);
+        ReadPlayer2MiddleLeftPunchInput(controls[13]);
+        ReadPlayer2MiddleRightPunchInput(controls[14]);
+        ReadPlayer2MiddleKickInput(controls[15]);
+        ReadPlayer2SpecialAttackInput(controls[16]);
+        ReadPlayer2JumpInput(controls[17]);
+        ReadPlayer2BendInput(controls[18]);
+        ReadPlayer2BlockInput(controls[19]);
     }
 
-    // player 1 punch text input
-    public void ReadPlayer1PunchInput(string s)
+
+    // --------------------- Player 1 -------------------------// 
+    // player 1 Upper Left Punch input
+    public void ReadPlayer1UpperLeftPunchInput(string s)
     {
-        if (s.Length > 1)
-        {
-            punch1 = s.Substring(0, 1).ToUpper();
-        }
-        else
-        {
-            punch1 = s.ToUpper();
+        string upper_left_punch1 = GetUpperSub(s);
 
-        }
-        if (punch1 == punch2 || punch1 == kick1 || punch1 == jump1 || punch1 == bend2 || punch1 == kick2 || punch1 == jump2 || punch1 == bend2)
-        {
-            punch1InputField.text = "..";
-        }
-        else
-        {
-            punch1InputField.text = punch1;
-        }
-
+        if (-1 != Array.IndexOf(controls, upper_left_punch1) && upper_left_punch1 != controls[0]) upper_left_punch1_Field.text = "..";
+        else upper_left_punch1_Field.text = upper_left_punch1;
     }
 
-    // player 1 kick text input
-    public void ReadPlayer1KickInput(string s)
+    // player 1 Upper Right Punch input
+    public void ReadPlayer1UpperRightPunchInput(string s)
     {
-        if (s.Length > 1)
-        {
-            kick1 = s.Substring(0, 1).ToUpper();
-        }
-        else
-        {
-            kick1 = s.ToUpper();
-        }
-        if (kick1 == kick2 || kick1 == punch1 || kick1 == jump1 || kick1 == bend1 || kick1 == punch2 || kick1 == jump2 || kick1 == bend2)
-        {
-            kick1InputField.text = "..";
-        }
-        else
-        {
-            kick1InputField.text = kick1;
-        }
+        string upper_right_punch1 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, upper_right_punch1) && upper_right_punch1 != controls[1]) upper_right_punch1_Field.text = "..";
+        else upper_right_punch1_Field.text = upper_right_punch1;
     }
 
-    // player 1 jump text input
+    // player 1 Upper Kick input
 
+    public void ReadPlayer1UpperKickInput(string s)
+    {
+        string upper_kick1 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, upper_kick1) && upper_kick1 != controls[2]) upper_kick1_Field.text = "..";
+        else upper_kick1_Field.text = upper_kick1;
+    }
+
+    // player 1 Middle Left Punch input
+    public void ReadPlayer1MiddleLeftPunchInput(string s)
+    {
+        string middle_left_punch1 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, middle_left_punch1) && middle_left_punch1 != controls[3]) middle_left_punch1_Field.text = "..";
+        else middle_left_punch1_Field.text = middle_left_punch1;
+    }
+
+
+    // player 1 Middle Right Punch input
+    public void ReadPlayer1MiddleRightPunchInput(string s)
+    {
+        string middle_right_punch1 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, middle_right_punch1) && middle_right_punch1 != controls[4]) middle_right_punch1_Field.text = "..";
+        else middle_right_punch1_Field.text = middle_right_punch1;
+    }
+
+    // player 1 Middle Kick input
+    public void ReadPlayer1MiddleKickInput(string s)
+    {
+        string middle_kick1 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, middle_kick1) && middle_kick1 != controls[5]) middle_kick1_Field.text = "..";
+        else middle_kick1_Field.text = middle_kick1;
+    }
+
+    // player 1 Special Attack input
+    public void ReadPlayer1SpecialAttackInput(string s)
+    {
+        string special_attack1 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, special_attack1) && special_attack1 != controls[6]) special_attack1_Field.text = "..";
+        else special_attack1_Field.text = special_attack1;
+    }
+
+    // player 1 Jump input
     public void ReadPlayer1JumpInput(string s)
     {
-        if (s.Length > 1)
-        {
-            jump1 = s.Substring(0, 1).ToUpper();
-        }
-        else
-        {
-            jump1 = s.ToUpper();
-        }
-        if (jump1 == jump2 || jump1 == punch1 || jump1 == kick1 || jump1 == bend1 || jump1 == punch2 || jump1 == kick2 || jump1 == bend2)
-        {
-            jump1InputField.text = "..";
-        }
-        else
-        {
-            jump1InputField.text = jump1;
-        }
+        string jump1 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, jump1) && jump1 != controls[7]) jump1_Field.text = "..";
+        else jump1_Field.text = jump1;
     }
 
-    // player 1 bend text input
+    // player 1 bend input
     public void ReadPlayer1BendInput(string s)
     {
-        if (s.Length > 1)
-        {
-            bend1 = s.Substring(0, 1).ToUpper();
-        }
-        else
-        {
-            bend1 = s.ToUpper();
-
-        }
-        if (bend1 == bend2 || bend1 == punch1 || bend1 == kick1 || bend1 == jump1 || bend1 == punch2 || bend1 == kick2 || bend1 == jump2)
-        {
-            bend1InputField.text = "..";
-
-        }
-        else
-        {
-            bend1InputField.text = bend1;
-        }
+        string bend1 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, bend1) && bend1 != controls[8]) bend1_Field.text = "..";
+        else bend1_Field.text = bend1;
     }
 
-
-    // player 2 punch text input
-    public void ReadPlayer2PunchInput(string s)
+    // player 1 block input
+    public void ReadPlayer1BlockInput(string s)
     {
-        if (s.Length > 1)
-        {
-            punch2 = s.Substring(0, 1).ToUpper();
-        }
-        else
-        {
-            punch2 = s.ToUpper();
-        }
-        if (punch2 == punch1 || punch2 == bend2 || punch2 == kick2 || punch2 == jump2 || punch2 == kick1 || punch2 == jump1 || punch2 == bend1)
-        {
-            punch2InputField.text = "..";
-        }
-        else
-        {
-            punch2InputField.text = punch2;
-        }
+        string block1 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, block1) && block1 != controls[9]) block1_Field.text = "..";
+        else block1_Field.text = block1;
     }
 
-    // player 2 kick text input
-    public void ReadPlayer2KickInput(string s)
+    // ---------------------- Player 2 ------------------------  // 
+
+    // player 2 Upper Left Punch input
+    public void ReadPlayer2UpperLeftPunchInput(string s)
     {
-        if (s.Length > 1)
-        {
-            kick2 = s.Substring(0, 1).ToUpper();
-        }
-        else
-        {
-            kick2 = s.ToUpper();
-        }
-        if (kick2 == kick1 || kick2 == bend2 || kick2 == punch2 || kick2 == jump2 || kick2 == punch1 || kick2 == jump1 || kick2 == bend1)
-        {
-            kick2InputField.text = "..";
-        }
-        else
-        {
-            kick2InputField.text = kick2;
-        }
+        string upper_left_punch2 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, upper_left_punch2) && upper_left_punch2 != controls[10]) upper_left_punch2_Field.text = "..";
+        else upper_left_punch2_Field.text = upper_left_punch2;
     }
 
-    // player 2 jump text input
+    // player 2 Upper Right Punch input
+    public void ReadPlayer2UpperRightPunchInput(string s)
+    {
+        string upper_right_punch2 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, upper_right_punch2) && upper_right_punch2 != controls[11]) upper_right_punch2_Field.text = "..";
+        else upper_right_punch2_Field.text = upper_right_punch2;
+    }
+
+    // player 2 Upper Kick input
+    public void ReadPlayer2UpperKickInput(string s)
+    {
+        string upper_kick2 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, upper_kick2) && upper_kick2 != controls[12]) upper_kick2_Field.text = "..";
+        else upper_kick2_Field.text = upper_kick2;
+    }
+
+    // player 2 Middle Left Punch input
+    public void ReadPlayer2MiddleLeftPunchInput(string s)
+    {
+        string middle_left_punch2 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, middle_left_punch2) && middle_left_punch2 != controls[13]) middle_left_punch2_Field.text = "..";
+        else middle_left_punch2_Field.text = middle_left_punch2;
+    }
+
+    // player 2 Middle Right Punch input
+    public void ReadPlayer2MiddleRightPunchInput(string s)
+    {
+        string middle_right_punch2 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, middle_right_punch2) && middle_right_punch2 != controls[14]) middle_right_punch2_Field.text = "..";
+        else middle_right_punch2_Field.text = middle_right_punch2;
+    }
+
+    // player 2 Middle Kick input
+    public void ReadPlayer2MiddleKickInput(string s)
+    {
+        string middle_kick2 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, middle_kick2) && middle_kick2 != controls[15]) middle_kick2_Field.text = "..";
+        else middle_kick2_Field.text = middle_kick2;
+    }
+
+    // player 2 Special Attack input
+    public void ReadPlayer2SpecialAttackInput(string s)
+    {
+        string special_attack2 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, special_attack2) && special_attack2 != controls[16]) special_attack2_Field.text = "..";
+        else special_attack2_Field.text = special_attack2;
+    }
+
+    // player 2 Jump input
     public void ReadPlayer2JumpInput(string s)
     {
-        if (s.Length > 1)
-        {
-            jump2 = s.Substring(0, 1).ToUpper();
-        }
-        else
-        {
-            jump2 = s.ToUpper();
-        }
-        if (jump2 == jump1 || jump2 == bend2 || jump2 == punch2 || jump2 == kick2 || jump2 == punch1 || jump2 == kick1 || jump2 == bend1)
-        {
-            jump2InputField.text = "..";
-        }
-        else
-        {
-            jump2InputField.text = jump2;
-        }
+        string jump2 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, jump2) && jump2 != controls[17]) jump2_Field.text = "..";
+        else jump2_Field.text = jump2;
     }
 
     // player 2 bend text input
     public void ReadPlayer2BendInput(string s)
     {
-        if (s.Length > 1)
-        {
-            bend2 = s.Substring(0, 1).ToUpper();
-        }
-        else
-        {
-            bend2 = s.ToUpper();
-        }
-        if (bend2 == bend1 || bend2 == jump2 || bend2 == punch2 || bend2 == kick2 || bend2 == punch1 || bend2 == kick1 || bend2 == jump1)
-        {
-            bend2InputField.text = "..";
-        }
-        else
-        {
-            bend2InputField.text = bend2;
-        }
+        string bend2 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, bend2) && bend2 != controls[18]) bend2_Field.text = "..";
+        else bend2_Field.text = bend2;
+    }
+
+    // player 2 Block input
+    public void ReadPlayer2BlockInput(string s)
+    {
+        string block2 = GetUpperSub(s);
+        if (-1 != Array.IndexOf(controls, block2) && block2 != controls[19]) block2_Field.text = "..";
+        else block2_Field.text = block2;
     }
 }
--- Assets/Scripts/SoundSettings.cs
+++ Assets/Scripts/SoundSettings.cs
@@ -7,6 +7,9 @@ public class SoundSettings : MonoBehaviour
     public AudioSource AudioSource;
     private float musicVolume = 0f;
 
+    [SerializeField] private GameObject soundSettingsWindow;
+    [SerializeField] private GameObject controlSettingsWindow;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,12 @@ public class SoundSettings : MonoBehaviour
     public void updateVolume(float volume)
     {
         musicVolume = volume;
+    }
+
+    public void toControl()
+    {
+        soundSettingsWindow.SetActive(false);
+        controlSettingsWindow.SetActive(true);
     }
 
 }
