9e07a44e17beaeba36126d8262d14b176360683b
Dave-Sama <davech1228@gmail.com>
2022-04-08 13:56:09 +0300

    added new KeyCode variables

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -11,6 +11,31 @@ public class DataManager : MonoBehaviour
     public int Damage { get; set; }
     public bool IsAttacking { get; set; } // Wether the player attacks or the opponent attacks
     public bool IsPlayer { get; set; } // Wether the player gets hit or the opponent gets hit to know who lost health
+    
+    // Keycodes for player 1 control
+
+    public KeyCode upper_left_punch1_Keycode { get; set; }
+    public KeyCode upper_right_punch1_Keycode { get; set; }
+    public KeyCode upper_kick1_Keycode { get; set; }
+    public KeyCode middle_left_punch1_Keycode { get; set; }
+    public KeyCode middle_right_punch1_Keycode { get; set; }
+    public KeyCode middle_kick1_Keycode { get; set; }
+    public KeyCode special_attack1_Keycode { get; set; }
+    public KeyCode jump1_Keycode { get; set; }
+    public KeyCode bend1_Keycode { get; set; }
+    public KeyCode block1_Keycode { get; set; }
+
+    // Keycodes for player 2 control
+    public KeyCode upper_left_punch2_Keycode { get; set; }
+    public KeyCode upper_right_punch2_Keycode { get; set; }
+    public KeyCode upper_kick2_Keycode { get; set; }
+    public KeyCode middle_left_punch2_Keycode { get; set; }
+    public KeyCode middle_right_punch2_Keycode { get; set; }
+    public KeyCode middle_kick2_Keycode { get; set; }
+    public KeyCode special_attack2_Keycode { get; set; }
+    public KeyCode jump2_Keycode { get; set; }
+    public KeyCode bend2_Keycode { get; set; }
+    public KeyCode block2_Keycode { get; set; }
     public string AttackName { get; set; } // Which attack is used
     public bool IsPlayerDead { get; set; }
     public bool IsOpponentDead { get; set; }
